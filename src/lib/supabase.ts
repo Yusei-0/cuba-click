bf95221a4e10e5ce6a29b5b7fbc17a7e54f6e41c
Yusei- <acmnhc@gmail.com>
2026-02-16 14:38:32 -0500

    fix: env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 import type { Database } from "../types/database.types";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://pmoiftzrylwkklplhymz.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtb2lmdHpyeWx3a2tscGxoeW16Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzExMzU5OTEsImV4cCI6MjA4NjcxMTk5MX0.7I230AskXEhzsjZBAOGeel-QiW58eGnDiXmTkhYO1D8";
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
